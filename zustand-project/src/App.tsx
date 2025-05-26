401dd15b8c6cfa3bb9af821feecdf2e7595403df
jafar-x <jafarsadikdev@gmail.com>
2025-05-26 13:02:12 +0600

    make recipe app with zustand

---
+++ zustand-project/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecipeApp from './components/RecipeApp';
+
+const App = () => {
+  return (
+    <div>
+      {/* <h1 className="text-3xl font-bold underline">Hello world</h1> */}
+      <RecipeApp />
+    </div>
+  );
+};
+
+export default App;
