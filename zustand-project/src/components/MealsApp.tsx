fa85b97abccbdd06dc9f7d63d1054da609282ed1
jafarsadikdev@gmail.com <jafarx@Jafars-MacBook-Air.local>
2025-05-29 10:48:05 +0600

    meal app initial commit

--- zustand-project/src/components/MealsApp.tsx
+++ zustand-project/src/components/MealsApp.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useMealStore } from '../mealStore';
 
 const MealsApp = () => {
   const { meals, searchQuery, setMeals, setQuery } = useMealStore();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(
+          'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood'
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  }, [setMeals]);
   return <div>MealsApp</div>;
 };
 
